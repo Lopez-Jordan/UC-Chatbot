f86954b986fa18ad75bca471a8d1c98f9def8f2e
Jordan Lopez <jordanlopezemail@gmail.com>
2023-11-27 16:47:05 -0800

    updated functionality of code and chatbot

--- client/src/Chatbot.jsx
+++ client/src/Chatbot.jsx
@@ -1,63 +1,73 @@
-import { useState, useEffect } from 'react';
-import {getResponse} from './chatbot/LLM.jsx';
+import { useState } from 'react';
+import { getResponse } from './chatbot/LLM.jsx';
 import './Chatbot.css';
 
+const MAX_CHARACTERS = 300;
+
 export default function Chatbot() {
-  const [message, setMessage] = useState("");
-  const [response, setResponse] = useState("");
+  const [userMessage, setUserMessage] = useState("");
   const [fullChat, setFullChat] = useState([]);
+
+  const handleInputChange = (e) => {
+    const inputText = e.target.value;
+    if (inputText.length <= MAX_CHARACTERS) {
+      setUserMessage(inputText);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const result = await getResponse(message);
-      setResponse(result);
-      setFullChat(prevFullChat => [
-        ...prevFullChat,
-        { role: 'user', content: message },
+      const result = await getResponse(userMessage);
+
+      setFullChat([
         { role: 'chatbot', content: result },
+        { role: 'user', content: userMessage },
+        ...fullChat,
       ]);
-      setResponse("");
-      setMessage("");
+
     } catch (error) {
       console.error(error);
     }
+
+    setUserMessage("");
   };
 
-  useEffect(() => {
-    // gather things from local storage
-    // load the 5 array items into the chatbot window
-  }, [])
+  const charactersRemaining = MAX_CHARACTERS - userMessage.length;
 
-    return (
-        <div className='main'>
-            <div id='chats'>
-                {fullChat.map((chat, index) => (
-                    <div key={index} className={`message ${chat.role}`}>
-                        {chat.content}
-                    </div>
-                ))}
-            </div>
-            <form onSubmit={handleSubmit} className="input-container">
-                <textarea
-                    onChange={(e) => setMessage(e.target.value)}
-                    placeholder='Message here'
-                    id="inputText"
-                    value={message}
-                />
-                <button
-                    disabled={!message.length}
-                    type="submit"
-                    id="submitButton"
-                    style={{ opacity: !message.length ? 0.4 : 1 }} // Set opacity based on message length
-                >
-                    <svg width="28" height="28" viewBox="0 0 24 24" fill="none" className="text-white dark:text-black">
-                        <path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-                    </svg>
-                </button>
-            </form>
-              
+  return (
+    <div className='main'>
+      <div id='chats'>
+        {fullChat.map((chat, index) => (
+          <div key={index} className={`message ${chat.role}`}>
+            {chat.content}
+          </div>
+        ))}
+      </div>
+
+      <form onSubmit={handleSubmit} className="input-container">
+        <textarea
+          onChange={handleInputChange}
+          placeholder='Message here'
+          id="inputText"
+          value={userMessage}
+        />
+        <div style={{height: '100%'}}>
+          <div className="char-counter">{charactersRemaining}</div>
+          <button
+            disabled={!userMessage.length}
+            type="submit"
+            id="submitButton"
+            style={{ opacity: !userMessage.length ? 0.4 : 1 }}
+          >
+            <svg width="28" height="28" viewBox="0 0 24 24" fill="none" className="text-white dark:text-black">
+              <path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+            </svg>
+          </button>
+
         </div>
-    );
+      </form>
+    </div>
+  );
 }
