5e2ef005346944dce7a9084ba48e07ffb18afa61
Jordan Lopez <jordanlopezemail@gmail.com>
2023-11-29 22:54:00 -0800

    update again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
           </div>
           <img id="emblem" src="/emblem.png" alt="" />
           <div style={{height: '200px'}}></div>
-          <p className='powered'>*powered by Chat GPT 3.5-turbo</p>
+          <p className='powered'>*powered by Chat GPT 3.5-turbo*</p>
         </div>
         <div className="half2">
             <Chatbot />
