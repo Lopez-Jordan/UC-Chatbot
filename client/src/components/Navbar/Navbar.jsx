c171ecec9e31d4edff8977968e1962a6dcd2e913
Jordan Lopez <jordanlopezemail@gmail.com>
2023-12-14 00:50:45 -0800

    updated styles with button

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,69 +1,85 @@
 import './Navbar.css';
-import { LogInContext } from "../../App";
+import { LogInContext } from '../../App';
 import { useGoogleLogin } from '@react-oauth/google';
-import { useState, useContext } from 'react';
+import { useContext, useState, useRef } from 'react';
 
 export default function () {
+  const [userLoggedIn, setUserLoggedIn] = useContext(LogInContext);
+  const [isPopupVisible, setPopupVisible] = useState(false);
 
-    const [userLoggedIn, setUserLoggedIn] = useContext(LogInContext);
-
-    const login = useGoogleLogin({
-        onSuccess: async (response) => {
-            try {
-                const res = await fetch('https://www.googleapis.com/oauth2/v3/userinfo' , {
-                    headers : {
-                        Authorization : `Bearer ${response.access_token}`,
-                    },
-                });
-                if (res.ok) {
-                    const resUserData = await res.json();
-
-                    setUserLoggedIn({
-                        loggedIn: true,
-                        name: `${resUserData.name}`,
-                        pic: `${resUserData.picture}`
-                    })
-
-
-                    console.log('User Information:', resUserData);
-                } else {
-                    console.error('Error fetching user information:', res.status, res.statusText);
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        },
-        onError: () => {
-            console.log('Login Failed');
+  const login = useGoogleLogin({
+    onSuccess: async (response) => {
+      try {
+        const res = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+          headers: {
+            Authorization: `Bearer ${response.access_token}`,
           },
-    })
+        });
+        if (res.ok) {
+          const resUserData = await res.json();
 
-    const handleSignOut = () => {
-        setUserLoggedIn({
-            loggedIn: false,
-            name: "",
-            pic: ""
-        })
+          setUserLoggedIn({
+            loggedIn: true,
+            name: `${resUserData.name}`,
+            pic: `${resUserData.picture}`,
+          });
+
+          console.log('User Information:', resUserData);
+        } else {
+          console.error('Error fetching user information:', res.status, res.statusText);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onError: () => {
+      console.log('Login Failed');
+    },
+  });
+
+  const handleSignOut = () => {
+    setUserLoggedIn({
+      loggedIn: false,
+      name: '',
+      pic: '',
+    });
+    setPopupVisible(false);
+  };
+
+  const togglePopup = () => {
+    setPopupVisible(!isPopupVisible);
+  };
+
+  const renderProfilePopup = () => {
+    if (isPopupVisible) {
+      return (
+        <div className="profile-popup">
+          <button onClick={handleSignOut}>Sign Out</button>
+        </div>
+      );
     }
+    return null;
+  };
+
 
     return (
         <>
-            <div className='navbar'>
-                {(userLoggedIn.loggedIn) ? ( // Change here
-                    <>
-                        <div onClick={handleSignOut} className='profile'>
-                            <img id="profileImage" src={userLoggedIn.pic} alt="" />
-                            <div className='infoContainer'>
-                                <p className='smallText'>signed in as</p>
-                                <h4>{userLoggedIn.name}</h4>
-                            </div>
+            <div className="navbar">
+                {userLoggedIn.loggedIn ? (
+                    <div className="profile" onClick={togglePopup}>
+                        <img id="profileImage" src={userLoggedIn.pic} alt="" />
+                        <div className="infoContainer">
+                            <p className="smallText">signed in as</p>
+                            <h4>{userLoggedIn.name}</h4>
                         </div>
-                    </>
+                        {renderProfilePopup()}
+                    </div>
                 ) : (
-                    <button className="signIn" onClick={() => login()}>SIGN IN</button>
+                    <button className="signIn" onClick={() => login()}>
+                        SIGN IN
+                    </button>
                 )}
             </div>
-
         </>
     );
 }
